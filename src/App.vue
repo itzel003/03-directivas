9604287d73df494793a9a72ff09ca3feba6d447a
Dianaandrade2022 <a3521110157@alumno.uttehuacan.edu.mx>
2022-09-29 08:40:22 -0500

    Directivas-Main

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="p-3 bg-light m-5">
+    <h1>Universidad Tecnologica de Tehuacán</h1>
+    <h2>Aplicaciones Orientadas a Servicios</h2>
+    <h3>Grupo 4B</h3>
+    <h1>Directivas en VUE.js</h1>
+  </div>
+    <hr />
+  <h2>V-text</h2>
+  <span class="text-primary" v-text= "'Hola desde la directiva v-text'"></span>
+  <br>
+  <span>Hola desde la directiva v-text</span>
+  <br>
+  <span class="text-primary" v-text= "titulo"></span>
+  <br>
+  <span class="text-success">Esta es una trasmutacion de texto {{titulo}}, con una variable prop </span>
+  <br>
+  <span class="text-primary" v-text= "titulo"></span>
+  <br><br>
+  
+  </div>
+  </template>
+  
+  <script>
+    export default{
+      data: () => ({
+        titulo: "Hola desde VUE.js",
+      }),
+    };
+  </script>
+  
+  <style scoped>
+  div{
+    text-align: center;
+    margin: 0px 2rem;
+  }
+  </style>
